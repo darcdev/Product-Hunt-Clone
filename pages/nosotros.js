e7dfe35aaa5532d123fd51976d14b3539195d10a
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2020-12-14 12:48:59 -0500

    proyecto base

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,11 +1,13 @@
+import React from "react";
 import Layout from "../components/Layouts/Layout";
-
-export default function Home() {
+const Nosotros = () => {
   return (
     <div>
       <Layout>
-        <h1>Inicio</h1>
+        <h1>Nosotros</h1>
       </Layout>
     </div>
   );
-}
+};
+
+export default Nosotros;
